3152626aaf5d8dd2c91efdd863add3d52fc8b02d
TolgaYazan <tolgayazan08@hotmail.com>
2022-12-28 18:48:36 +0300

    secondcommiit

---
+++ src/main/java/com/uniyaz/objectOriented/App.java
@@ -0,0 +1,18 @@
+package com.uniyaz.objectOriented;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+
+
+
+
